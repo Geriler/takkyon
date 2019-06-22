86c1aa13ded7063f83322ddeaf453ec2c87b5bb7
Alexander <alexanderkoltygin@gmail.com>
2019-06-22 20:05:07 +0600

    Change main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,6 @@
+<?php
+$user = Auth::user();
+?>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -68,12 +71,12 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Домашняя</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">Авторизация</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Регистрация</a>
                         @endif
                     @endauth
                 </div>
@@ -81,17 +84,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Takyon
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @if (is_object($user))
+                        @if($user->is_admin)
+                            <a href="{{ route('questions.index') }}">Вопросы</a>
+                            <a href="{{ route('progress.index') }}">Прогресс</a>
+                        @endif
+                    @endif
                 </div>
             </div>
         </div>
