7ba9fce672a7747c5ae19ae5e1a08a73364be7bf
Alexander <alexanderkoltygin@gmail.com>
2019-06-23 12:31:48 +0600

    Add shuffle in answer

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -8,17 +8,28 @@
     <form method="post" action="{{ route('map.check', $id) }}">
         {{ csrf_field() }}
     @foreach($questions as $question)
+			<?php
+				$answers = [
+					$question->answer1,
+					$question->answer2,
+					$question->answer3,
+                ];
+				if (!is_null($question->answer4)) array_push($answers, $question->answer4);
+				if (!is_null($question->answer5)) array_push($answers, $question->answer5);
+				shuffle($answers);
+			?>
+
         <div class="question">
             <h3 class="question-title">{{ $question->question }}</h3>
             <div>
-                <input id="1{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $question->answer1 }}"> <label for="1{{ $question->id }}">{{ $question->answer1 }}</label><br>
-                <input id="2{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $question->answer2 }}"> <label for="2{{ $question->id }}">{{ $question->answer2 }}</label><br>
-                <input id="3{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $question->answer3 }}"> <label for="3{{ $question->id }}">{{ $question->answer3 }}</label><br>
+                <input id="1{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $answers[0] }}"> <label for="1{{ $question->id }}">{{ $answers[0] }}</label><br>
+                <input id="2{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $answers[1] }}"> <label for="2{{ $question->id }}">{{ $answers[1] }}</label><br>
+                <input id="3{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $answers[2] }}"> <label for="3{{ $question->id }}">{{ $answers[2] }}</label><br>
                 @if($question->answer4)
-                    <input id="4{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $question->answer4 }}"> <label for="4{{ $question->id }}">{{ $question->answer4 }}</label><br>
+                    <input id="4{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $answers[3] }}"> <label for="4{{ $question->id }}">{{ $answers[3] }}</label><br>
                 @endif
                 @if($question->answer5)
-                    <input id="5{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $question->answer5 }}"> <label for="5{{ $question->id }}">{{ $question->answer5 }}</label><br>
+                    <input id="5{{ $question->id }}" type="radio" name="answer{{ $question->id }}" value="{{ $answers[4] }}"> <label for="5{{ $question->id }}">{{ $answers[4] }}</label><br>
                 @endif
             </div>
         </div>
