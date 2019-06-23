ad728d2d6a0d1d69341df5dbc1c5d97687e38608
Alexander <alexanderkoltygin@gmail.com>
2019-06-23 19:00:45 +0600

    Add surname and translate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->surname }} {{ mb_substr(Auth::user()->name, 0, 1, 'UTF-8') }}. <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
