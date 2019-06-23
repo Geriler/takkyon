70e8aeca327864fe2ba51dc7726c2e58a07d47de
Alexander <alexanderkoltygin@gmail.com>
2019-06-23 13:54:22 +0600

    Change display questions

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -7,10 +7,14 @@
 @section('content')
     <a class="btn btn-primary" href="{{ route('questions.create') }}">Создать вопрос</a>
     @if(count($questions) > 0)
+        <table class="table table-striped">
         @foreach($questions as $question)
-            <div class="question">
-                <div class="question-title"><a href="{{ route('questions.show', $question->id) }}">({{ $question->id }}) {{ $question->question }}</a></div>
-            </div>
+            <tr>
+                <td>
+                    <a href="{{ route('questions.show', $question->id) }}">({{ $question->id }}) {{ $question->question }}</a>
+                </td>
+            </tr>
         @endforeach
+        </table>
     @endif
 @endsection
