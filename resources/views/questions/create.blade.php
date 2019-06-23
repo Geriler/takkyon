93ffc728252559d0c093f98d9ac19d53e1771bc5
Alexander <alexanderkoltygin@gmail.com>
2019-06-23 14:01:53 +0600

    Change label

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -11,21 +11,22 @@
         <form action="{{ route('questions.store') }}" method="post" class="form-vertical">
             {{ csrf_field() }}
             <input type="text" name="question" class="form-control" value="{{ old('question') }}" placeholder="Вопрос">
+            <label>Уровень сложности</label>
             <select name="level">
                 @for($i = 1; $i <= 5; $i++)
                     <option value="{{ $i }}">{{ $i }}</option>
                 @endfor
             </select>
             <input type="text" name="answer1" class="form-control" value="{{ old('answer1') }}" placeholder="Правильный вариант ответа">
-            <textarea name="comment1" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ old('comment1') }}</textarea>
-            <input type="text" name="answer2" class="form-control" value="{{ old('answer2') }}" placeholder="Не правильный вариант ответа">
-            <textarea name="comment2" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ old('comment2') }}</textarea>
-            <input type="text" name="answer3" class="form-control" value="{{ old('answer3') }}" placeholder="Не правильный вариант ответа">
-            <textarea name="comment3" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ old('comment3') }}</textarea>
-            <input type="text" name="answer4" class="form-control" value="{{ old('answer4') }}" placeholder="Не правильный вариант ответа">
-            <textarea name="comment4" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ old('comment4') }}</textarea>
-            <input type="text" name="answer5" class="form-control" value="{{ old('answer5') }}" placeholder="Не правильный вариант ответа">
-            <textarea name="comment5" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ old('comment5') }}</textarea>
+            <textarea name="comment1" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ old('comment1') }}</textarea>
+            <input type="text" name="answer2" class="form-control" value="{{ old('answer2') }}" placeholder="Неправильный вариант ответа">
+            <textarea name="comment2" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ old('comment2') }}</textarea>
+            <input type="text" name="answer3" class="form-control" value="{{ old('answer3') }}" placeholder="Неправильный вариант ответа">
+            <textarea name="comment3" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ old('comment3') }}</textarea>
+            <input type="text" name="answer4" class="form-control" value="{{ old('answer4') }}" placeholder="Неправильный вариант ответа">
+            <textarea name="comment4" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ old('comment4') }}</textarea>
+            <input type="text" name="answer5" class="form-control" value="{{ old('answer5') }}" placeholder="Неправильный вариант ответа">
+            <textarea name="comment5" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ old('comment5') }}</textarea>
             <button class="btn btn-primary">Сохранить</button>
             <a class="btn btn-primary" href="{{ route('questions.index') }}">Назад</a>
         </form>
--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -12,21 +12,22 @@
             {{ method_field('put') }}
             {{ csrf_field() }}
             <input type="text" name="question" class="form-control" value="{{ $question->question }}" placeholder="Вопрос">
+            <label>Уровень сложности</label>
             <select name="level">
                 @for($i = 1; $i <= 5; $i++)
                     <option @if($question->level == $i) selected @endif value="{{ $i }}">{{ $i }}</option>
                 @endfor
             </select>
             <input type="text" name="answer1" class="form-control" value="{{ $question->answer1 }}" placeholder="Правильный вариант ответа">
-            <textarea name="comment1" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ $question->comment1 }}</textarea>
-            <input type="text" name="answer2" class="form-control" value="{{ $question->answer2 }}" placeholder="Не правильный вариант ответа">
-            <textarea name="comment2" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ $question->comment2 }}</textarea>
-            <input type="text" name="answer3" class="form-control" value="{{ $question->answer3 }}" placeholder="Не правильный вариант ответа">
-            <textarea name="comment3" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ $question->comment3 }}</textarea>
-            <input type="text" name="answer4" class="form-control" value="{{ $question->answer4 }}" placeholder="Не правильный вариант ответа">
-            <textarea name="comment4" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ $question->comment4 }}</textarea>
-            <input type="text" name="answer5" class="form-control" value="{{ $question->answer5 }}" placeholder="Не правильный вариант ответа">
-            <textarea name="comment5" cols="30" rows="10" class="form-control" placeholder="Пояснения к ответу">{{ $question->comment5 }}</textarea>
+            <textarea name="comment1" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ $question->comment1 }}</textarea>
+            <input type="text" name="answer2" class="form-control" value="{{ $question->answer2 }}" placeholder="Неправильный вариант ответа">
+            <textarea name="comment2" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ $question->comment2 }}</textarea>
+            <input type="text" name="answer3" class="form-control" value="{{ $question->answer3 }}" placeholder="Неправильный вариант ответа">
+            <textarea name="comment3" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ $question->comment3 }}</textarea>
+            <input type="text" name="answer4" class="form-control" value="{{ $question->answer4 }}" placeholder="Неправильный вариант ответа">
+            <textarea name="comment4" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ $question->comment4 }}</textarea>
+            <input type="text" name="answer5" class="form-control" value="{{ $question->answer5 }}" placeholder="Неправильный вариант ответа">
+            <textarea name="comment5" cols="30" rows="10" class="form-control" placeholder="Пояснение к ответу">{{ $question->comment5 }}</textarea>
             <button class="btn btn-primary">Сохранить</button>
             <a class="btn btn-primary" href="{{ route('questions.show', $question->id) }}">Назад</a>
         </form>
